2cf9e7898a74988194911914a02e0fb1bf13fc9e
Roman Budai <roman1985buday@gmail.com>
2022-07-27 18:50:33 +0300

    fixed tabs markup

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,4 +1,5 @@
 const store = document.querySelector('.goods-store');
+const checkStock = document.querySelector('.goods-items-top_status__img');
 
 export function getTabs(url) {
   let data = url;
@@ -15,6 +16,11 @@ export function getTabs(url) {
 
 export function generateTabs(imgUrl, name, inStock, price, reviews, orders) {
   store.insertAdjacentHTML('beforeend', createTab(imgUrl, name, inStock, price, reviews, orders));
+  checkInStock();
+}
+
+function checkInStock(inStock) {
+
 }
 
 export function createTab(imgUrl, name, inStock, price, reviews, orders) {
@@ -29,17 +35,17 @@ export function createTab(imgUrl, name, inStock, price, reviews, orders) {
           <span class="goods-items-top_status__quantity">${inStock}</span>left in stock
         </span>
         <span class="goods-items-top_price title_fz14">Price:
-          <span class="goods-items-top_price__sum">${price}</span>$
+          <span class="goods-items-top_price__sum">${price} $</span>
         </span>
         <button class="btn goods-items-top_btn">Add to cart</button>
       </div>
 
       <div class="goods-items-bottom">
         <img class="goods-items-bottom_filledlike" loading="lazy" src="img/svg/like_filled_red.svg" alt="">
-        <span class="goods-items-bottom_reviews title_fz14">
-          <span class="goods-items-bottom_reviews__percent">${reviews}</span>Positive reviews
-          <br> Above avarage
-        </span>
+        <div class="goods-items-bottom_reviews title_fz14">
+          <span class="goods-items-bottom_reviews__percent"><span>${reviews}%</span>Positive reviews</span>
+          <span class="goods-items-bottom_reviews__above">Above avarage</span>
+        </div>
         <span class="goods-items-bottom_order title_fz14">
           <span class="goods-items-bottom_order__quantity">${orders}</span>orders
         </span>
