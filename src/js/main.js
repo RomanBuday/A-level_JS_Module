f8789a4f25838e500116407e42f580b25e46513d
Roman Budai <roman1985buday@gmail.com>
2022-07-27 14:32:56 +0300

    getTabs fixed bugs

--- src/js/main.js
+++ src/js/main.js
@@ -3,52 +3,35 @@ import {items} from "./items.js";
 document.addEventListener('DOMContentLoaded', function () {
   const store = document.querySelector('.goods-store');
 
-  // async function getTabs(url, fn) {
-  //   const resp = await fetch(url);
-  //   const data = await resp.json();
-  //   console.log(data);
-  //   data.forEach((el, index) => {
-  //       generateTabs(
-  //           data[index].id,
-  //           data[index].imgUrl,
-  //           data[index].name,
-  //           data[index].orderInfo.inStock,
-  //           data[index].price,
-  //           data[index].orderInfo.reviews,
-  //           data[index].orderInfo.orders);
-  //       });
-  //   fn();
-  // }
-
-  function getTabs() {
-    for (const index of items) {
-      generateTabs (
-        index.id,
-        index.imgUrl,
-        index.name,
-        index.orderInfo.inStock,
-        index.price,
-        index.orderInfo.reviews,
-        index.orderInfo.orders,
-      );
-    }
+  function getTabs(url) {
+    let data = url;
+    data.forEach((el, index) => {
+      generateTabs(
+        data[index].imgUrl,
+        data[index].name,
+        data[index].orderInfo.inStock,
+        data[index].price,
+        data[index].orderInfo.reviews,
+        data[index].orderInfo.orders);
+    });
+    console.log(data);
   }
 
   getTabs(items);
 
-  function createTab(data) {
+  function createTab(imgUrl, name, inStock, price, reviews, orders) {
     return `
       <div class="goods-items">
         <div class="goods-items-top">
           <img class="goods-items-top_like" loading="lazy" src="img/svg/like_empty.svg" alt="">
-          <img class="goods-items-top_img" loading="lazy" src="${data.imgUrl}" alt="">
-          <h2 class="goods-items-top_title title title_fz24">${data.name}</h2>
+          <img class="goods-items-top_img" loading="lazy" src="./img/${imgUrl}" alt="">
+          <h2 class="goods-items-top_title title title_fz24">${name}</h2>
           <span class="goods-items-top_status title_fz14">
             <img class="goods-items-top_status__img" loading="lazy" src="img/svg/check.svg" alt="">
-            <span class="goods-items-top_status__quantity">${data.inStock}</span>left in stock
+            <span class="goods-items-top_status__quantity">${inStock}</span>left in stock
           </span>
           <span class="goods-items-top_price title_fz14">Price:
-            <span class="goods-items-top_price__sum">${data.price}</span>$
+            <span class="goods-items-top_price__sum">${price}</span>$
           </span>
           <button class="btn goods-items-top_btn">Add to cart</button>
         </div>
@@ -56,18 +39,18 @@ document.addEventListener('DOMContentLoaded', function () {
         <div class="goods-items-bottom">
           <img class="goods-items-bottom_filledlike" loading="lazy" src="img/svg/like_filled_red.svg" alt="">
           <span class="goods-items-bottom_reviews title_fz14">
-            <span class="goods-items-bottom_reviews__percent">${data.reviews}</span>Positive reviews <br> Above avarage
+            <span class="goods-items-bottom_reviews__percent">${reviews}</span>Positive reviews
+            <br> Above avarage
           </span>
           <span class="goods-items-bottom_order title_fz14">
-            <span class="goods-items-bottom_order__quantity">${data.orders}</span>orders
+            <span class="goods-items-bottom_order__quantity">${orders}</span>orders
           </span>
         </div>
       </div>`;
   }
 
-  function generateTabs(id, imgUrl, name, inStock, price, reviews, orders) {
-    store.insertAdjacentHTML('beforeend', createTab(id, imgUrl, name, inStock, price, reviews, orders));
+  function generateTabs(imgUrl, name, inStock, price, reviews, orders) {
+    store.insertAdjacentHTML('beforeend', createTab(imgUrl, name, inStock, price, reviews, orders));
   }
-
 
 });
