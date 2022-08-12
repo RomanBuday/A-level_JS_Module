0db7b31429d6b06b093293c40bfda695554ebf5b
Roman Budai <roman1985buday@gmail.com>
2022-08-12 14:54:25 +0300

    add onFiterChangefunc

--- src/js/filter.js
+++ src/js/filter.js
@@ -39,31 +39,16 @@ export function showFilter() {
       }
     }
   });
+
+  document.querySelectorAll('.filter-content input[type="checkbox"]')
+  .forEach(checkbox => checkbox.addEventListener('change', onFilterChange));
+  document.querySelectorAll('.filter-content > .content-price input')
+  .forEach(input => input.addEventListener('input', onPriceFilterChange));
 }
 
 //-----------------------------------------------
 
-// function priceRange() {
-//   let maxPrice = 0;
-//   let minPrice = 0;
-
-//   let items = document.querySelectorAll('.goods-item');
-
-//   let arrPrice = [];
-
-//   items.forEach(item => {
-//     let priceStr = item.querySelector('.price-sum').textContent;
-//     arrPrice.push(parseInt(priceStr.match(/\d+/)));
-//     maxPrice = Math.max(...arrPrice);
-//     minPrice = Math.min(...arrPrice);
-//   });
-
-//   document.querySelector(`.price-inner_input[data-price = 'minPrice']`).placeholder = minPrice;
-//   document.querySelector(`.price-inner_input[data-price = 'maxPrice']`).placeholder = maxPrice;
-
-// }
-
-export function priceRange() {
+function priceRange() {
   let maxPrice = 0;
   let minPrice = 0;
 
@@ -134,8 +119,58 @@ const filterFunctions = {
   }
 };
 
-export function priceFilter() {
+export function onFilterChange() {
+  const currentFilteresSelected = getAllFiltersSelected();
+  console.log('-- filteres selected', currentFilteresSelected);
 
-  priceRange();
+  const minSelected = document.querySelector(`.filter-content > .content-price input[data-price="minPrice"]`).value;
+  const maxSelected = document.querySelector(`.filter-content > .content-price input[data-price="maxPrice"]`).value;
+
+  const domItems = document.querySelectorAll('.goods-item');
+
+  domItems.forEach(domItem => {
+    const id = domItem.querySelector('.top-item_img').getAttribute('data-id');
+    const item = getItemById(id);
+    if (!item) {
+      console.error(`!!missing item ${id}`);
+    }
+
+    domItem.style.display = '';
+
+    Object.keys(currentFilteresSelected).forEach(filterName => {
+      const filtersValue = currentFilteresSelected[filterName];
+      if (filtersValue.length > 0) {
+        const filterCallback = filterFunctions[filterName];
+        const showFlag = filterCallback(filtersValue, item);
+        if (!showFlag) {
+          domItem.style.display = 'none';
+        }
+      }
+    });
+
+    if ((minSelected !== '' && item.price < Number(minSelected)) || (maxSelected !== '' && item.price > Number(maxSelected))) {
+      domItem.style.display = 'none';
+    }
+
+  });
 }
+
+function onPriceFilterChange(e) {
+  // const min = document.querySelector(`.filter-content > .content-price input[data-price="minPrice"]`);
+  // const max = document.querySelector(`.filter-content > .content-price input[data-price="maxPrice"]`);
+  // if (!min.value || Number(min.value) < minPrice) {
+  //   min.value = minPrice;
+  // }
+  // if (!max.value || Number(max.value) > maxPrice) {
+  //   max.value = maxPrice;
+  // }
+
+  // if (min.value > max.value) {
+  //   max.value = min.value;
+  // }
+
+  onFilterChange(e);
+}
+
+
 
